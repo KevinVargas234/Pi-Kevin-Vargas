6fd3dac5cd9d7b6bad19dd5bcd6ba7a15314e74b
KevinVargas234 <92541862+KevinVargas234@users.noreply.github.com>
2022-11-08 12:26:45 -0500

    final commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render,screen } from "@testing-library/react";
+import Card from "../src/components/Card/Card"
+import { Link,BrowserRouter } from "react-router-dom";
+describe("card",()=>{
+    test("deberia tener un elemento Link si la prop use_link es true",()=>{
+        render(
+        <BrowserRouter>
+          <Card name="pug" temperaments = {["curious","playfull"]} weight = {[10,20]} id = {0} use_link ={true} />
+        </BrowserRouter>
+        );
+        expect(screen.getByRole("link")).toBeInTheDocument();
+    });
+    test("no deberia tener un elemento Link si la prop use_link es false",()=>{
+      render(
+      <BrowserRouter>
+        <Card name="pug" temperaments = {["curious","playfull"]} weight = {[10,20]} id = {0} use_link ={false} />
+      </BrowserRouter>
+      );
+      expect(screen.queryByRole("link")).toBeNull();
+    });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+})
